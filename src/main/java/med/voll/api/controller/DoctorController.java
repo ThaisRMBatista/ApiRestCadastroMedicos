48752f7cab347b794838f4a4434cb3fc99bb9a95
Thais Regina Mendes Batista <thais.batista@maximatech.com.br>
2023-08-28 17:13:46 -0300

    add transactional on the method post, for spring know that is a transactional method

--- src/main/java/med/voll/api/controller/DoctorController.java
+++ src/main/java/med/voll/api/controller/DoctorController.java
@@ -4,6 +4,7 @@ import med.voll.api.domain.DTO.DoctorDTO;
 import med.voll.api.domain.Doctor;
 import med.voll.api.services.DoctorService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,6 +18,7 @@ public class DoctorController {
     private DoctorService service;
 
     @PostMapping
+    @Transactional
     public Doctor createDoctor(@RequestBody DoctorDTO doctorDTO) {
         return service.createDoctors(doctorDTO);
     }
